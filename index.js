3cae616371f62cf771d8660c0051de76e95da6fd
unknown <kaitlandevans@gmail.com>
2022-09-30 14:12:26 -0500

    C:/Program Files/Git/ updated & cors added

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
-let restaurantsRoutes = require("./routes/restaurants")
-//and use it
-app.use(restaurantsRoutes)
+let restaurantsRoutes = require("./routes/restaurants");
+app.use(restaurantsRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 app.listen(PORT, () => console.log(`Listening on port ${PORT} `));
